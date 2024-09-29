a17ee162ec2cd2752fe0ad93d6861e61cf8a2c16
Haseeb Khan <haseebkhanbettani@gmail.com>
2024-09-29 19:46:38 +0500

    5th File

---
+++ Practice_Question/5_simple_bookshop_struct.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+struct book
+{
+    string name;
+    double price;
+    long pages;
+};
+
+void read(book a[], int i)
+{
+    cout << "the book " << (i + 1) << endl;
+    cout << "Enter book name: ";
+    cin.ignore();
+    getline(cin, a[i].name);
+    cout << "Enter book price: ";
+    cin >> a[i].price;
+    cout << "Enter book pages: ";
+    cin >> a[i].pages;
+}
+
+int main()
+{
+    book a[3];
+
+    for (int i = 0; i < 3; i++)
+    {
+        read(a, i);
+    }
+
+    cout << "===========================================\n";
+    for (int i = 0; i < 3; i++)
+    {
+        cout << "The book " << i + 1 << " name is: " << a[i].name << endl;
+        cout << "Price is: " << a[i].price << endl;
+        cout << "Total pages is: " << a[i].pages << endl;
+    }
+
+    return 0;
+}
