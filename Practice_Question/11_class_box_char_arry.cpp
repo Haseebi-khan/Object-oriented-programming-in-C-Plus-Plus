4562f37bae88f5ddc3c6c5b7d0454a1a2e1f4feb
Haseeb Khan <haseebkhanbettani@gmail.com>
2024-10-03 13:39:44 +0500

    Eidited code.

--- Practice_Question/11_class_box_char_arry.cpp
+++ Practice_Question/11_class_box_char_arry.cpp
@@ -1,97 +1,72 @@
+// Question:
+
+// Write a C++ program that defines a class StringArray to manage character arrays (strings) effectively. The program should meet the following requirements:
+
+// Implement a default constructor and a parameterized constructor that accepts a string (character array) and stores it in a private member variable str.
+// Define a method setString that takes a string as input and stores it in the class's internal array.
+// Define a method printString that outputs the stored string to the console.
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// In the main function, prompt the user to input a name (string), store it using the setString method, and then display the name using the printString method.
+// Ensure that the class can handle strings up to 19 characters in length (excluding the null terminator).
+
 #include <iostream>
 using namespace std;
 
-class box
+//                                                  DONE
+
+class StringArray
 {
-    double volumn=0;
+    char str[20];
 
 public:
-    double lenght, width, height;
-    // member function
-    double getdata(double lenght, double height, double width);
-    void displaydata()
+    StringArray() {}
+    StringArray(const char arr[])
     {
-        cout << "The volumn is: " << volumn << endl;
+        int i = 0;
+        while (arr[i] != '\0' && i < 20)
+        {
+            str[i] = arr[i];
+            i++;
+        }
+        str[i] = '\0';
     }
-};
+    void setStr(const char* arr)
+    {
+        int i = 0;
+        while (arr[i] != '\0' && i <20)
+        {
+            str[i] = arr[i];
+            i++;
+        }
+        str[i] = '\0';
+    }
+    void displaySet()
+    {
+        cout << "The String is: " << str <<endl;
+    }
 
-double box ::getdata(double lenght1, double height1, double width1)
-{
-    return volumn = lenght * height * width;
-}
+};
 
 int main()
 {
-    box b;
-    double height , lenght, width;
 
-    cout << "Enter the height: ";
-    cin >> b.height;
-    cout << "Enter the lenght: ";
-    cin >> b.lenght;
-    cout << "Enter the width: ";
-    cin >> b.width;
+    cout << "Enter First String (without spaces): ";
+    char str[20];
+    char str2[20];
+    cin >> str;
+    cout << "Enter 2nd String (without spaces): ";
+    cin >> str2;
 
-    b.getdata(b.height, b.lenght, b.width);
+    StringArray _1;
+    StringArray _2;
 
-    b.displaydata();
+    _1.setStr(str);
+    _2.setStr(str2);
 
-    return 0;
-}
+    _1.displaySet();
+    _2.displaySet();
 
-////////////////////////////////////////////////////////////////////////////////////////
-
-#include <iostream>
-#include <cstring>
-
-using namespace std;
-int i = 0;
-
-class arry
-{
-        char arr[20];
-    public:
-        arry() {}
-        arry(const char a[])
-        {
-            int i = 0;
-            while (a[i] != '\0' && i < 19) {
-                arr[i] = a[i];
-                i++;
-            }
-            arr[i] = '\0';
-            
-        }
-        void call(const char a[])
-        {
-            int i = 0;
-            while (a[i] != '\0' && i < 19) {
-                arr[i] = a[i];
-                i++;
-            }
-            
-            
-        }
-        void getname()
-        {
-            cout << arr <<endl;
-        }
-};
-
-
-int main()
-{
-    arry i;
-    cout << "Enter the name : ";
-    char name[20];
-    cin >> name;
-
-    i.call(name);
-
-    i.getname();
-    
-
-
-    
     return 0;
 }
