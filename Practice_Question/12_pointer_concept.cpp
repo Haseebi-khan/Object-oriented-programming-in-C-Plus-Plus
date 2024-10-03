de578f177e63a0ae50263febd4719ab7af05c645
Haseeb Khan <haseebkhanbettani@gmail.com>
2024-10-03 13:40:05 +0500

    Eidited code.

--- Practice_Question/12_pointer_concept.cpp
+++ Practice_Question/12_pointer_concept.cpp
@@ -1,130 +1,96 @@
-// #include <iostream>
-
-// using namespace std;
-
-// void greatest(int *x, int *y, int *z)
-// {
-//     if (*x > *y && *x > *z)
-//     {
-//         cout << "largest value is: " << *x << endl;
-//     }
-//     else if (*y > *x && *y > *z)
-//     {
-//         cout << "largest value is: " << *y << endl;
-//     }
-//     else if (*z > *y && *z > *y)
-//     {
-//         cout << "largest value is: " << *z << endl;
-//     }
-// }
-
-// void greatests(int *x, int size)
-// {
-//     int max = x[0];
-
-//     for (int i = 0; i < size; i++)
-//     {
-//         if (x[i] < max)
-//         {
-//             max = x[i];
-//         }
-//     }
-
-//     cout << "Maximum no is: " << max;
-// };
-
-// int main()
-// {
-//     int x[5] = {
-//         2, 6, 5, 3, 4};
-
-//     int *p = x;
-
-//     // greatest(p,q,r);
-
-//     greatest(p, 5);
-
-//     return 0;
-// }
-
-///////////////////////////////////////
 #include <iostream>
-
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
-double factorial(int *x)
+//////////////////////////      Done
+
+string greatestIndex(int *numptr)
 {
-    double fact = 1;
-    if (*x == 0)
+    string highIndexValue;
+    if (numptr[0] > numptr[1] && numptr[0] > numptr[2])
     {
-        return 1;
+        highIndexValue = "First Index Value is highest. ";
     }
-    else
+    else if (numptr[1] > numptr[0] && numptr[1] > numptr[2])
     {
-        for (int i = *x; i > 1; i--)
-        {
-            fact *= i;
-        }
-        return fact;
+        highIndexValue = "2nd Index Value is highest. ";
     }
+    else if (numptr[2] > numptr[1] && numptr[2] > numptr[0])
+    {
+        highIndexValue = "3rd Index Value is highest. ";
+    }
+
+    return highIndexValue;
 }
 
+double hightestValue(int *numptr, int size)
+{
+    int max = numptr[0];
 
-void reverse(int* num){
-    int last = *num % 10;
+    for (int i = 0; i < size; i++)
+    {
+        if (numptr[i] > max)
+        {
+            max = numptr[i];
+        }
+    }
+    return max;
+};
 
-    int seclast = (*num / 10 ) % 10;
+void reverse(int &num)
+{
+    int last = num % 10;
 
-    int sec = (*num / 100) % 10;
+    int seclast = (num / 10) % 10;
 
-    int one = (*num / 1000); 
+    int sec = (num / 100) % 10;
 
-    *num = 888;
+    int one = (num / 1000);
 
-    cout << one << sec << seclast << last << endl;
+    cout << last << seclast << sec <<  one << endl;
 }
 
 int main()
 {
-    // cout << "Enter the value: ";
-    // int f;
-    // cin >> f;
-    // int *ptr = &f;
+    srand(time(0));
 
-    // cout << factorial(ptr);
+    int numArry[3];
 
-    int num = 9331;
-    int* ptr=&num;
+    for (int i = 0; i < 3; i++)
+    {
+        numArry[i] = rand() % 10000 + 1;
+    }
 
-    reverse(ptr);
-    cout << "sadsaddddddddddddddddd   " << num; 
+    for (int i = 0; i < 3; i++)
+    {
+        cout << numArry[i] << " ";
+    }
+    cout << endl;
 
+    int *numptr = numArry;
 
-    // while (num != 0)
-    // {
-    //     int digit = num % 10;
-    //     reverse = reverse * 10 + digit;
-    //     num = num / 10;
-    // }
+    int(*wholeBlockOfArray)[3] = &numArry; // how to use it.
 
-    
+    string highestIndex = greatestIndex(numptr);
+    int highestValue = hightestValue(numptr, 3);
 
+    cout << highestIndex << endl;
+    cout << "Heighest Value is: " << highestValue << endl;
 
+    cout << "WholeBlockOfArray: " << endl;
+    for (int i = 0; i < 3; i++)
+    {
+        cout << (*wholeBlockOfArray)[i] << " ";
+    }
+    cout << endl;
+    cout << endl;
+    cout << "Reversed Number: ";
+    reverse(highestValue);
 
+    int *arr[] = {&numArry[0], &numArry[1], &numArry[2]};
+    cout << *arr[0] << " " << *arr[1] << " " << *arr[2] << endl;
 
+    cout << endl;
     return 0;
 }
-
-//=======================================================================================
-
-
-// #include <iostream>
-// using namespace std;
-// int main() {
-
-//     int a = 5, b = 10, c = 15;
-//     int *arr[] = {&a, &b, &c};
-//     cout << *arr[1];
-    
-//     return 0;
-// }
