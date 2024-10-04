14cff0ad0d91101a0e412dba38e23da73099b0ec
Haseeb Khan <haseebkhanbettani@gmail.com>
2024-10-04 18:58:19 +0500

    7th File

--- Practice_Question/7_dyna_arr_malloc.cpp
+++ Practice_Question/7_dyna_arr_malloc.cpp
@@ -3,41 +3,6 @@ using namespace std;
 
 ////////////////////////////////////////////////////???                        DONE
 
-int main()
-{
-
-    int *array = (int *)malloc(5 * sizeof(int));
-    string *array2 = new string[5];
-
-    // using malloc
-    for (int i = 0; i < 5; i++)
-    {
-        array[i] = i + 1;
-    }
-
-    for (int i = 0; i < 5; i++)
-    {
-        cout << array[i] << endl;
-    }
-
-    free(array);
-
-    // using new
-    for (int i = 0; i < 5; i++)
-    {
-        array2[i] = "String " + to_string(i + 1);
-    }
-    for (int i = 0; i < 5; i++)
-    {
-        cout << array2[i] << endl;
-    }
-    delete[] array2;
-    // both methods are valid for allocating memory dynamically, but using new is generally considered the better practice in C++.
-    // it automatically calls the constructor of the object type and automatically handles memory deallocation when the object goes out of scope,
-    // making it simpler and safer to use.
-    return 0;
-}
-
 // int main()
 // {
 
@@ -57,3 +22,38 @@ int main()
 
 //     return 0;
 // }
+
+int main() {
+    
+    int  *array = (int *) malloc(5 * sizeof(int));
+    string *array2 = new string[5];
+    
+    // using malloc
+    for (int i = 0; i < 5; i++) {
+        array[i] = i + 1;
+
+    }
+    
+    for (int i = 0; i < 5; i++) {
+        cout << array[i] << endl;
+    }
+    
+    free(array);
+
+    // using new
+    for (int i = 0; i < 5; i++) {
+        array2[i] = "String " + to_string(i + 1) ;
+    }
+    for (int i = 0; i < 5; i++) {
+        cout << array2[i] << endl;
+    }
+    delete[] array2;
+
+    // both methods are valid for allocating memory dynamically, but using new is generally considered the better practice in C++.
+    // it automatically calls the constructor of the object type and automatically handles memory deallocation when the object goes out of scope,
+    // making it simpler and safer to use.
+
+
+
+    return 0;
+}
